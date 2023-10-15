792726a8d8d71ca6a48d486d3875da973b31f9d2
Vinicius0422 <guedescosta123@hotmail.com>
2023-10-15 13:41:29 -0300

    First Commit

---
+++ src/main/java/br/vinicius/projects/controleDeProcessos/ControleDeProcessosApplication.java
@@ -0,0 +1,13 @@
+package br.vinicius.projects.controleDeProcessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeProcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeProcessosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/vinicius/projects/controleDeProcessos/ControleDeProcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vinicius.projects.controleDeProcessos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeProcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
